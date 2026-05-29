5a203b401f77da3104d93f8e80daef780a368afd
Kevin Liao <kevincliao@google.com>
2026-05-28 21:39:56 -0700

    Fix error_callback.ts passing function instead of actual values to logger
    This commit also adds tests for it to prevent such errors from happening again.

--- lib/logging/logging.ts
+++ lib/logging/logging.ts
@@ -66,7 +66,7 @@ export function logError(unknownError: unknown): JjError {
     return error;
   }
   error.isLogged = true;
-  logger?.error(error, getGlobalErrorCallback?.());
+  logger?.error(error, getGlobalErrorCallback?.()?.());
   return error;
 }
 
