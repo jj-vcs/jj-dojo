390eda38158c294e473780330d45f3a35b61d27c
Kevin Liao <kevincliao@google.com>
2026-05-28 21:39:56 -0700

    Fix error_callback.ts passing function instead of actual values to logger
    This commit also adds tests for it to prevent such errors from happening again.

---
+++ lib/logging/logging.test.ts
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2026 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {expect} from 'vitest';
+import {vscode_test} from '../../testing/vscode_test';
+import {JjError} from '../error/error';
+import {
+  setGlobalLogger,
+  removeGlobalLogger,
+  logError,
+  logAndShowInternalError,
+} from './logging';
+import {OutputChannelLogger} from './output_channel_logger';
+import {
+  removeGlobalErrorCallback,
+  setGlobalErrorCallback,
+} from './error_callback';
+import {
+  setGlobalFeedbackProvider,
+  removeGlobalFeedbackProvider,
+} from './feedback';
+
+vscode_test('logError should log and set isLogged status', async ({log}) => {
+  const logger = new OutputChannelLogger();
+  setGlobalLogger(logger);
+
+  const err = new JjError('some error');
+  const result = logError(err);
+
+  expect(result).toBe(err);
+  expect(result.isLogged).toBe(true);
+  expect(log.messages).toEqual([['ERROR', 'some error', undefined]]);
+
+  removeGlobalLogger();
+});
+
+vscode_test('global error callback should append to error', async ({log}) => {
+  const logger = new OutputChannelLogger();
+  setGlobalLogger(logger);
+  setGlobalErrorCallback(() => 'extra info');
+
+  const err = new JjError('some error');
+  const result = logError(err);
+
+  expect(result).toBe(err);
+  expect(result.isLogged).toBe(true);
+  expect(log.messages).toEqual([['ERROR', 'some error', 'extra info']]);
+
+  removeGlobalErrorCallback();
+  removeGlobalLogger();
+});
+
+vscode_test(
+  'global feedback provider should invoke when internal error is logged',
+  async () => {
+    const logger = new OutputChannelLogger();
+    setGlobalLogger(logger);
+
+    let feedbackFiled = false;
+    setGlobalFeedbackProvider(() => {
+      feedbackFiled = true;
+    });
+
+    const err = new JjError('internal error');
+    const result = logAndShowInternalError(err);
+
+    expect(result).toBe(err);
+    expect(result.isInternalError).toBe(true);
+    expect(result.isShownToUser).toBe(true);
+    expect(feedbackFiled).toBe(true);
+
+    removeGlobalFeedbackProvider();
+    removeGlobalLogger();
+  },
+);
